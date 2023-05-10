15406e11a036fad888118585242b462e1d09a2a7
deathkun26 <pdminhtoan@gmail.com>
2023-05-11 00:27:54 +0700

    remove gramformer and tesseract

--- professional_summary/generator/gramformer.py
+++ professional_summary/generator/gramformer.py
@@ -1,15 +1,22 @@
-from gramformer import Gramformer
+# from gramformer import Gramformer
 
-# 0 = detector, 1 = highlighter, 2 = corrector, 3 = all
+# # 0 = detector, 1 = highlighter, 2 = corrector, 3 = all
+
+# class GrammarCorrect:
+#     def __init__(self):
+#         self.gf = Gramformer(models=1, use_gpu=False)
+
+#     def correct(self, sentences):
+#         outputs = []
+#         for sentence in sentences:
+#             res = self.gf.correct(sentence,  max_candidates=1)
+#             print(res)
+#             outputs += list(res)
+#         return outputs
 
 class GrammarCorrect:
     def __init__(self):
-        self.gf = Gramformer(models=1, use_gpu=False)
-
+        print("!!! use fake gramformer !!!")
+    
     def correct(self, sentences):
-        outputs = []
-        for sentence in sentences:
-            res = self.gf.correct(sentence,  max_candidates=1)
-            print(res)
-            outputs += list(res)
-        return outputs
+        return sentences
