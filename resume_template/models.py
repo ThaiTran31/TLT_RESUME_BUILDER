54e8c989559245313a7f2a1496d76616ec7fe7c3
ThaiTran31 <thaitrancs31@gmail.com>
2023-05-11 11:54:33 +0700

    [FIX] revise resume template module for thumbnail

--- resume_template/models.py
+++ resume_template/models.py
@@ -4,7 +4,7 @@ from django.db import models
 class ResumeTemplate(models.Model):
     title = models.CharField(max_length=50)
     description = models.TextField(null=True, blank=True)
-    thumbnail = models.ImageField(upload_to='images/', null=True, blank=True)
+    thumbnail = models.ImageField(upload_to='resume_template_thumbnails/', null=True, blank=True)
     CATEGORY_CHOICES = [
         ('creative', 'Creative'),
         ('simple', 'Simple'),
@@ -15,8 +15,3 @@ class ResumeTemplate(models.Model):
 
     def __str__(self):
         return self.title
-
-    def get_thumbnail(self):
-        if self.thumbnail:
-            return 'http://127.0.0.1:8000' + self.thumbnail.url
-        return 'http://127.0.0.1:8000' + '/media/images/' + self.title.lower() + '.png'
